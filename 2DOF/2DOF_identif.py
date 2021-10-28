47b8445082c95cfc12845746ffa2a38170181dcd
ThibaultAjas <thibault.ajas@laposte.net>
2021-10-28 16:31:05 +0200

    [IMP] Step 1 to 3 are working!

--- 2DOF/2DOF_identif.py
+++ 2DOF/2DOF_identif.py
@@ -4,9 +4,7 @@ from pinocchio.robot_wrapper import RobotWrapper
 import matplotlib.pyplot as plt
 import pinocchio as pin
 import numpy as np
-import math
 import os
-
 
 currentDir = os.getcwd()
 os.chdir('../')
@@ -14,54 +12,52 @@ workingDir = os.getcwd()
 
 # urdf directory path
 package_path = workingDir
-urdf_path = package_path + '/robots/urdf/planar_2DOF.urdf'
+urdf_path    = package_path + '/robots/urdf/planar_2DOF.urdf'
+
+# Step 1 - load model, create robot model and create robot data
+
 robot = RobotWrapper()
-
-#robot.BuildFromURDF(urdf,path,verbose=True)
-robot.initFromURDF(urdf_path,package_path,verbose=True)
-#print("MODEL DU ROBOT\n",robot.model)
-#print("VISUAL MODEL ",robot.visual_model)
-
+robot.initFromURDF(urdf_path, package_path,verbose=True)
 robot.initViewer(loadModel=True)
 robot.display(robot.q0)
-# loading robot datas (data,model,NQ,NV,Njoint)
-data = robot.data
-model = robot.model
-NQ = robot.nq #number of joint angle 
-NV = robot.nv #number of joint velocity 
-NJOINT = robot.model.njoints
-gv = robot.viewer.gui
 
+data   = robot.data
+model  = robot.model
+NQ     = robot.nq                       # number of joint angle 
+NV     = robot.nv                       # number of joint velocity 
+NJOINT = robot.model.njoints            # number of links
+gv     = robot.viewer.gui
+
+# Step 2 - generate inertial parameters for all joint 
 names = []
-for i in [1 ,2]:
-    print(i)
+for i in range(NJOINT):
     names += ['m'+str(i), 'mx'+str(i), 'my'+str(i), 'mz'+str(i), 'Ixx'+str(i), 'Ixy'+str(i), 'Iyy'+str(i), 'Izx'+str(i), 'Izy'+str(i), 'Izz'+str(i)]
 
-print("tout les noms : ",names,"taille des noms ",len(names))
-dict = {}
-for i in range(len(names)):
-    print(i)
-    dict[names[i]] = 0
+values = []
+for inertia in model.inertias:
+    values.extend(inertia.toDynamicParameters())
 
-print("dictionnaire \n",dict)
-print("taille dictionnaire :",len(dict))
+print(len(values))
 
-print(NQ)
-samples = 100 # number of samples
-q = np.random.rand(NQ,samples)*(np.pi) - np.pi/2# -pi<q<2pi
-dq = np.random.rand(NQ,samples)*10
-ddq = np.ones((NQ,samples))
+inertialParameters = {names[i] : values[i] for i in range(len(names))}
 
-tau = np.zeros((NQ,samples))
-#print(tau)
-for i in range(samples):
-    tau[:,i] = np.transpose(pin.rnea(model,data,q[:,i],ddq[:,i],ddq[:,i])) #calcul du tau réel 
-print("size : ",tau.size)
+# Step 3 - Generate input and output - 100 samples
+
+nbSamples = 100 # number of samples
+
+# Generate 100 inputs
+q   = np.random.rand(NQ, nbSamples) * np.pi - np.pi/2  # -pi/2 < q   < pi/2
+dq  = np.random.rand(NQ, nbSamples) * 10               #     0 < dq  < 10
+ddq = np.ones((NQ, nbSamples))                         #         ddq = 1
+
+# Generate 100 outputs --- WIP (A voir avec le doctorant)
+tau = np.array([np.transpose(pin.rnea(model, data, q[:, i], ddq[:, i], ddq[:, i])) for i in range(nbSamples)])
+print(tau.shape)
+
+# Step 4 - Create IDM with pinocchio
 
 
 
-
-print("press enter to continue")
-input()
+# print("press enter to continue")
+# input()
 gv.deleteNode('world', True)  # name, all=True
-#on fais bouger le robot pour le test 
