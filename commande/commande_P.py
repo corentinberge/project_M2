d6d8cd90bfebc8df564a88e2ad3728b12aeb47fd
corentinberge <corentinberge@gmail.com>
2021-11-03 16:42:19 +0100

    Edit file with changes to add command law

--- commande/commande_P.py
+++ commande/commande_P.py
@@ -1,6 +1,7 @@
 from numpy.core.defchararray import join
 from numpy.core.fromnumeric import transpose
 import pinocchio as pin
+from pinocchio.deprecated import forwardDynamics
 from pinocchio.utils import *
 from pinocchio.visualize import GepettoVisualizer
 from pinocchio.robot_wrapper import RobotWrapper
@@ -12,16 +13,17 @@ import time
 import os
 
 #Simulation de la trajectoire
-def simulateurTraj(N,robot,IDX):
+def simulateurTraj(N):
     dt = 1e-2
     a0,a1,a2 = 0,1,2
     X = np.zeros((N,7))
     for i in range(N-1):
         q,dq = loiPoly(a0,a1,a2,i*dt)
         q = np.array(q)
-        robot.forwardKinematics(q) #update joint 
+        loi(q,X)
+        #robot.forwardKinematics(q) #update joint 
         pin.updateFramePlacements(robot.model,robot.data) #update frame placement  
-        X[i,:] = pin.SE3ToXYZQUAT(robot.data.oMf[IDX])
+        X[i,:] = pin.SE3ToXYZQUAT(robot.data.oMf[ID_F])
         robot.display(q)
         time.sleep(dt)
     print(X.shape)
@@ -39,14 +41,35 @@ def jacobienneValidation(J,Xp):
 def calculDotX(X):
     print("test")
     
+#Loi de commande du système
+def loi(Xd,X):
+    E = []
+    print(len(X),X)
+    print(len(Xd),Xd)
+    for i in range(len(Xd)-1):
+        E[i] = X[i] - Xd[i]
+        prop(E,0.5)
+        jacob(X)
+        mvt_Robot(X,Xd)
+        robot.forwardKinematics(Xd)
+
+
 #Passage de X dans un proportionnel
 def prop(X,p):
 	for i in X:
 		i = i*p
 
 #Passade de X dans une jacobienne
-def jacob(X,J):
-	return
+def jacob(X):
+    j = pin.jacobianSubtreeCenterOfMass(model,data,2)
+    for i in range(3) :
+        if(j[i][0] != 0.):
+            X[i] *= j[i]
+
+def mvt_Robot(X,Xd):
+    Xd = Xd + X
+
+
 
 #Main 
 #currentDir = os.getcwd()
@@ -72,3 +95,6 @@ NQ = robot.nq #number of joint angle
 NV = robot.nv #number of joint velocity 
 NJOINT = robot.model.njoints
 gv = robot.viewer.gui
+ID_F = robot.model.getFrameId("tcp")
+ID = robot.model.getJointId("tcp")-1
+simulateurTraj(1000)
