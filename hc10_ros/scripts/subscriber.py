2965f597d00ec764e56f1a401ca7c114da96c0e6
ThibaultAjas <thibault.ajas@laposte.net>
2022-03-15 10:49:33 +0100

    more testing

--- hc10_ros/scripts/subscriber.py
+++ hc10_ros/scripts/subscriber.py
@@ -2,18 +2,21 @@ import rospy
 # from std_msgs.msg import String
 from sensor_msgs.msg import JointState
 
-f = open("data_torque.txt", "w")
+f = open("data/data_torque.txt", "w")
 
 def callback(data):
     # pos = data.position
     position_float = [float(i) for i in data.position]
     # efforts = data.effort
     efforts_float = [float(i) for i in data.effort]
+    # velocity = data.velocity
+    velocity_float = [float(i) for i in data.velocity]
     # print(efforts_float)
-    f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(
-    position_float[0],position_float[1],position_float[2],position_float[3],
-    position_float[4],position_float[5],efforts_float[0],efforts_float[1],
-    efforts_float[2],efforts_float[3],efforts_float[4],efforts_float[5]))
+    f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(
+        position_float[0], position_float[1], position_float[2], position_float[3], position_float[4], position_float[5],
+        velocity_float[0], velocity_float[1], velocity_float[2], velocity_float[3], velocity_float[4], velocity_float[5],
+        efforts_float[0], efforts_float[1], efforts_float[2], efforts_float[3], efforts_float[4], efforts_float[5])
+    )
     # rospy.loginfo("I heard %s",data.data)
     
 def listener():
--- hc10_ros/scripts/talker.py
+++ hc10_ros/scripts/talker.py
@@ -13,13 +13,12 @@ def talker():
     for i in range(0, 6):
         pub.append(rospy.Publisher('/motoman_hc10/joint'+str(i+1)+'_position_controller/command', Float64, queue_size=10))
         rospy.init_node('talker', anonymous=True)
-        rate = rospy.Rate(100) # 10hz
+        rate = rospy.Rate(100) # 100hz
 
     # Open file
-    f = open("data_speed.txt", "r")
+    f = open("data/all_position_data.txt", "r")
     l = f.readline()
     
-
     while not (rospy.is_shutdown() and len(l) != 0):
             
         # tmp = [f.readline() for i in range(1,50)]
@@ -37,7 +36,7 @@ def talker():
         q_data_float = [float(i) for i in q_data]
 
         for i in range(0, 6):
-            rospy.loginfo(q_data_float[i])
+            # rospy.loginfo(q_data_float[i])
             pub[i].publish(q_data_float[i])
         
         rate.sleep()
